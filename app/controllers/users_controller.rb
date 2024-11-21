5f4e80509815eba29abaac24fd3ddb4f9c3ca271
Charles Boucher <charlesb.ipag@gmail.com>
2024-11-21 19:49:33 +0100

    add section boats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def dashboard
     # Bookings the user has made
     @my_bookings = current_user.bookings.includes(:boat)
-
+    @my_boats = current_user.boats
     # Bookings made on the user's boats
     @boats_bookings = Booking.includes(:user, :boat).where(boat: current_user.boats)
     @my_boat_bookings = Booking.joins(:boat).where(boats: { user_id: current_user.id }).includes(:user, :boat)
