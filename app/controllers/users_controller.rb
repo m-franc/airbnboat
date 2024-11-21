78c614af20e0e3025e7c5b85bc4128374e9864af
Charles Boucher <charlesb.ipag@gmail.com>
2024-11-21 12:31:47 +0100

    fix my bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
     # Bookings made on the user's boats
     @boats_bookings = Booking.includes(:user, :boat).where(boat: current_user.boats)
+    @my_boat_bookings = Booking.joins(:boat).where(boats: { user_id: current_user.id }).includes(:user, :boat)
   end
-
-  
 end
