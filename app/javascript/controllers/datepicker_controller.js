ee811c58fd8b67276674d82f31087033a06823c3
Cyrill Semah <cyrillsemah@gmail.com>
2024-11-22 14:02:32 +0100

    Price affiché

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
 // Connects to data-controller="datepicker"
@@ -10,20 +10,28 @@ export default class extends Controller {
 
   connect() {
     const total = document.getElementById("price");
+
     flatpickr(this.element, {
       mode: "range",
       dateFormat: "Y-m-d",
       disable: this.datesValue,
       minDate: new Date(),
       onClose: () => {
-        const dates = this.element.value.split(" to ")
-        const oneDay = 24 * 60 * 60 * 1000;
-        const startDate = new Date(dates[0]);
-        const endDate = new Date(dates[1]);
-        const diffDays = Math.round(Math.abs((endDate - startDate) / oneDay));
-        const totalPrice = diffDays * this.priceValue
-        total.innerHTML = totalPrice
+        const dates = this.element.value.split(" to ");
+        if (dates.length === 2) {
+          const oneDay = 24 * 60 * 60 * 1000;
+          const startDate = new Date(dates[0]);
+          const endDate = new Date(dates[1]);
+          const diffDays = Math.round(Math.abs((endDate - startDate) / oneDay)) + 1;
+          const totalPrice = diffDays * this.priceValue;
+
+          // Ajout d'un délai pour synchroniser avec l'effet d'animation
+          setTimeout(() => {
+            total.textContent = `${totalPrice} €`;
+            total.classList.add("show"); // Ajoute la classe pour l'animation
+          }, 100); // Temps d'attente avant l'animation
+        }
       }
-    })
+    });
   }
 }
